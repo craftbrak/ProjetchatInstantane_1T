f3ce587a9994214ae217989d89fc437235fb5403
craftbrak <62942458+craftbrak@users.noreply.github.com>
2020-04-13 11:02:48 +0200

    upgrade du chat avec du jQuerry

--- js/chat.js
+++ js/chat.js
@@ -5,17 +5,12 @@ let Setion = {
 let Game = {
     id: 1
 }
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', initPage);
 
 function initPage() {
     //document.getElementById("formMessage").addEventListener("submit",TraiterFormMessage(this));
     updateChat();
-    setInterval(updateChat,1000);
+    setInterval(updateChat, 1000);
 }
 
 function TraiterFormMessage(formMessage) {
@@ -25,7 +20,7 @@ function TraiterFormMessage(formMessage) {
     envoiMsg.onload = function() {
         //console.log("msg envoyer");
         updateChat();
-        document.getElementById("formMessage").message.value=null;
+        document.getElementById("formMessage").message.value = null;
     };
     envoiMsg.send();
     return false;
@@ -38,14 +33,14 @@ function updateChat() {
     console.log("oupdate send");
     chatUpdate.onload = function() {
         let chat = JSON.parse(chatUpdate.responseText);
-       // chat.date = chat.date.toLocaleDateString(locales, {});
+        // chat.date = chat.date.toLocaleDateString(locales, {});
         //console.log(chat);
         let zoneChat = document.getElementById("divChat");
         let chatFinal = "";
         for (const element of chat) {
             //console.log(element.heure);
-             element.heure = element.heure.slice(11,-4);
-             //console.log(element.heure);
+            element.heure = element.heure.slice(11, -4);
+            //console.log(element.heure);
             chatFinal += "<p class='chatMessage' id='" + element.msgId + "'><span class='chatPseudo' >" + element.pseudo + "</span>" + "<span class='chatMessagContent'>" + element.msgContent + "</span>" + "<span class='chatTimeCode'>" + element.heure + "</span></p>"
 
         }
