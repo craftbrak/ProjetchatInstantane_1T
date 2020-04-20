6d65a1189c03cb6b3ceaf017c858eec4abe422ad
Arthur Schamroth <schamrotharthur@gmail.com>
2020-04-20 17:57:49 +0200

    id

--- js/modifier.js
+++ js/modifier.js
@@ -8,5 +8,6 @@ function initialiserFormulaire (){
 function remplirFormulaire(){
     let reponse = JSON.parse(this.responseText);
     console.log(reponse);
-    document.getElementById("formulaire").innerHTML += "<input type='text' placeholder='" + reponse.nom + "'>"
+    document.getElementById("formulaire").innerHTML += "<label for='idUser'>ID</label> <input name='idUser' type='text' " +
+        "placeholder='" + reponse[0].idUser + "'>"
 }
