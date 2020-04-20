7adc8949b4fa6076918bc7201f4ef078440748bb
Arthur Schamroth <schamrotharthur@gmail.com>
2020-04-20 17:13:10 +0200

    oui

---
+++ js/modifier.js
@@ -0,0 +1,11 @@
+function initialiserFormulaire (){
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "http://craftbrakddns.myddns.me:536/modification?idChoisi=780", true);
+    xhr.onload = remplirFormulaire;
+    xhr.send();
+}
+
+function remplirFormulaire(){
+    let reponse = JSON.parse(this.responseText);
+    console.log(reponse);
+}
