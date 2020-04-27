d95a70ebd97778886f3694517af2cd5a414fe50e
craftbrak <62942458+craftbrak@users.noreply.github.com>
2020-04-27 14:04:24 +0200

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -5,7 +5,7 @@ const product = urlParams.get('id')
 console.log(product);
 let idUser = product;
 
-let Game = {
+let Conv = {
     id: 1
 }
 document.addEventListener('DOMContentLoaded', initPage);
@@ -19,7 +19,7 @@ function initPage() {
 function TraiterFormMessage(formMessage) {
     let message = formMessage.message.value;
     let envoiMsg = new XMLHttpRequest;
-    envoiMsg.open("GET", "newMsg?msgContentVar=" + message + "&idUserVar=" + idUser + "&idGameVar=" + Game.id + "", true);
+    envoiMsg.open("GET", "newMsg?msgContentVar=" + message + "&idUserVar=" + idUser + "&idConvVar=" + Conv.id + "", true);
     envoiMsg.onload = function() {
         //console.log("msg envoyer");
         updateChat();
@@ -32,7 +32,7 @@ function TraiterFormMessage(formMessage) {
 
 function updateChat() {
     let chatUpdate = new XMLHttpRequest
-    chatUpdate.open("GET", "updateChat?idGameVar=" + Game.id, true);
+    chatUpdate.open("GET", "updateChat?idGameVar=" + Conv.id, true);
     console.log("oupdate send");
     chatUpdate.onload = function() {
         let chat = JSON.parse(chatUpdate.responseText);
@@ -54,7 +54,7 @@ function updateChat() {
 }
 $('document').ready(function() {
     $('#msg').each(function() {
-        $(this).height($(this).parent().height());
+
         $(this).width($(this).parent().width());
     });
 });
