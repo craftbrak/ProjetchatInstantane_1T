0f9ea484a042c24b9b865617162b1713e3aaf202
gironfran <gironfran@outlook.fr>
2020-05-02 14:15:57 +0200

    Update choixConv.js

--- js/choixConv.js
+++ js/choixConv.js
@@ -10,7 +10,7 @@ function initConvs() {
             let liste = '';
             JSON.parse(getConvs.responseText).forEach(conv => {
                 console.log(conv);
-                liste += '<div class=\"convListe\" id=\"' + conv.id + '\" href=\"http://craftbrakddns.myddns.me:536/play.html?id="' + conv.id + '\">' + conv.nom + '</div>';
+                liste += '<div class=\"convListe\" id=\"' + conv.id + '\" href=\"http://craftbrakddns.myddns.me:536/play.html?id=' + conv.id + '\">' + conv.nom + '</div>';
             });
             document.getElementById('listeConvs').innerHTML = liste;
             $('.convListe').click(goToConv);
