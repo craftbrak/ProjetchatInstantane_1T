1e1aa5aac21eb1fbc46054deb61bfbdcbc3aa896
gironfran <gironfran@outlook.fr>
2020-05-02 15:06:09 +0200

    Update choixConv.js

--- js/choixConv.js
+++ js/choixConv.js
@@ -3,28 +3,34 @@ document.addEventListener('DOMContentLoaded', initConvs);
 $(document).on('InitOver', initConvs);
 
 function initConvs() {
-    if (session.userId != null) {
-        let getConvs = new XMLHttpRequest;
-        getConvs.open('get', `userConvs?userId=${session.userId}`, true);
-        getConvs.onload = () => {
-            let liste = '';
-            JSON.parse(getConvs.responseText).forEach(conv => {
-                liste += '<div class=\"convListe\" id=\"' + conv.id + '\" href=\"http://craftbrakddns.myddns.me:536/play.html?id=' + conv.id + '\">' + conv.nom + '</div>';
-            });
-            document.getElementById('listeConvs').innerHTML = liste;
-            $('.convListe').click(goToConv);
-        }
-        getConvs.send();
+    créerListeConvs();
+    actualiserNomPage();
+}
+
+function créerListeConvs() {
+    let getConvs = new XMLHttpRequest;
+    getConvs.open('get', `userConvs?userId=${session.userId}`, true);
+    getConvs.onload = () => {
+        let liste = '';
+        JSON.parse(getConvs.responseText).forEach(conv => {
+            liste += '<div class=\"convListe\" id=\"' + conv.id + '\" href=\"http://craftbrakddns.myddns.me:536/play.html?id=' + conv.id + '\">' + conv.nom + '</div>';
+        });
+        document.getElementById('listeConvs').innerHTML = liste;
+        $('.convListe').click(goToConv);
     }
-    let getName = new XMLHttpRequest;
-        getName.open('get', `getName?id=${session.convUserId}`, true);
-        getName.onload = () => {
-            document.title = getName.responseText;
-        }
-        getName.send();
+    getConvs.send();
 }
 
 function goToConv(event) {
     let lien = event.target.getAttribute('href');
     window.location = lien;
+}
+
+function actualiserNomPage() {
+    let getName = new XMLHttpRequest;
+    getName.open('get', `getName?id=${session.convUserId}`, true);
+    getName.onload = () => {
+        document.title = getName.responseText;
+    }
+    getName.send();
 }
