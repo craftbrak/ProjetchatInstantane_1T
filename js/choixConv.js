cb3db998ee42ba677d7264518dfcc6934d193b0a
gironfran <gironfran@outlook.fr>
2020-05-02 12:16:40 +0200

    Update choixConv.js

--- js/choixConv.js
+++ js/choixConv.js
@@ -6,7 +6,11 @@ function initConvs() {
         let getConvs = new XMLHttpRequest;
         getConvs.open('get', `userConvs?userId=${conv.convUserId}`, true);
         getConvs.onload = () => {
-
+            let liste = '';
+            JSON.parse(getConvs.responseText).forEach(conv => {
+                liste += '<div class=\"convListe\" href=\"http://craftbrakddns.myddns.me:536/play.html?id=' + conv.id + '\">' + conv.nom + '</div>';
+            });
+            document.getElementById('listeConvs').innerHTML = liste;
         }
         getConvs.send();
     }
