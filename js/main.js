1947ac01b4e46ec5cf5a8a8b6bfe21a8e2645435
craftbrak <62942458+craftbrak@users.noreply.github.com>
2020-05-03 19:24:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(() => {
     $('#logo').click(() => {
-        window.location = "http://craftbrakddns.myddns.me:536/index.html"
-    })
+            window.location = "http://craftbrakddns.myddns.me:536/index.html"
+        })
+        /*const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        let userId = urlParams.get('id');
+        $('#modif').href = `./modificationProfil.html?id=${userId}`;
+        $.get(`getPseudo?id=${userId}`, (p) => { $('#iden').append(`Vous êtes connecté en tant que ${p}.`); });*/
 })
