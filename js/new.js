096f78032162a94f02b46accc7617306699d4681
gironfran <gironfran@outlook.fr>
2020-05-04 10:48:08 +0200

    q

--- js/new.js
+++ js/new.js
@@ -3,6 +3,11 @@ const urlParams = new URLSearchParams(queryString);
 let userId = urlParams.get('id');
 
 function formNewConv(formulaire) {
-    
+
     return false;
+}
+
+function setColor(select) {
+    select.removeClasss();
+    select.classList.add(select.value);
 }
