489bc1ed6d2cf1580624144ff02aec61eb71c222
gironfran <62607116+gironfran@users.noreply.github.com>
2020-05-05 15:40:12 +0200

    Update new.js

--- js/new.js
+++ js/new.js
@@ -2,6 +2,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let userId = urlParams.get('id');
 
+$(document).ready(initAlias);
+
 function formNewConv(formulaire) {
 
     return false;
@@ -10,4 +12,8 @@ function formNewConv(formulaire) {
 function setColor() {
     $('#color').removeClass();
     $('#color').addClass($('#color').value);
+}
+
+function initAlias() {
+    $.get(`getPseudo?id=${userId}`,(pseudo)=>{$('#iden').innerText = `Vous êtes connecté en tant que ${pseudo}.`;});
 }
