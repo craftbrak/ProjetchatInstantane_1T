b3529481f3a44fdc5910b58517a3174cee609dd1
gironfran <62607116+gironfran@users.noreply.github.com>
2020-05-08 16:02:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
     $('#logo').click(() => {
-            window.location = "./index.html"
-        })
-        /*const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        let userId = urlParams.get('id');
-        $('#modif').href = `./modificationProfil.html?id=${userId}`;
-        $.get(`getPseudo?id=${userId}`, (p) => { $('#iden').append(`Vous êtes connecté en tant que ${p}.`); });*/
+        window.location = "./index.html"
+    })
+    /*const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let userId = urlParams.get('id');
+    $('#modif').href = `./modificationProfil.html?id=${userId}`;
+    $.get(`getPseudo?id=${userId}`, (p) => { $('#iden').append(`Vous êtes connecté en tant que ${p}.`); });*/
 })
